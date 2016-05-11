e7572167c6ea43c7f23ca34aa4ffe02a0555e902
Lootgvfr <brdmonster@gmail.com>
2016-05-11 21:54:54 +0300

    registration

--- src/AppBundle/Controller/SignController.php
+++ src/AppBundle/Controller/SignController.php
@@ -2,6 +2,8 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Form\Register;
+use AppBundle\Entity\User;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
@@ -11,10 +13,39 @@ class SignController extends Controller
     /**
      * @Route("/sign", name="sign")
      */
-    public function homeAction(Request $request)
+    public function signAction(Request $request)
     {
-        return $this->render('sign.html.twig', array(
-        ));
+		$logger = $this->get('logger');
+		$user = new User();
+        $formRegister = $this->createForm(Register::class, $user);
+		if('POST' === $request->getMethod())
+		{
+			if ($request->request->has('register'))
+			{
+				$formRegister->handleRequest($request);
+				if ($formRegister->isSubmitted() && $formRegister->isValid()) 
+				{
+					$password = $this->get('security.password_encoder')
+						->encodePassword($user, $user->getPlainPassword());
+					$user->setPassword("lol");
+					$user->setGroup("User");
+					
+					$em = $this->getDoctrine()->getManager();
+					$em->persist($user);
+					$em->flush();
+					
+					return $this->redirectToRoute('sign');
+				}
+			}
+			else if ($request->request->has('login'))
+			{
+				// LOGIN FORM HERE
+			}
+		}
+
+        return $this->render('sign.html.twig', 
+			array('form' => $formRegister->createView())
+			);
     }
 	
 }
