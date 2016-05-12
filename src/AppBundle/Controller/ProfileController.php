8718d416d76a890ff5b6c5040fd30d9441b6ae2d
FanryLight <fanrylight@gmail.com>
2016-05-12 21:24:36 +0300

    profile1

--- src/AppBundle/Controller/ProfileController.php
+++ src/AppBundle/Controller/ProfileController.php
@@ -27,6 +27,24 @@ class ProfileController extends Controller
 				'group'    => $user->getGroup()
 			));
     }
+	/**
+     * @Route("/profile/edit", name="edit")
+     */
+    public function editAction()
+    {
+		$user = $this->getDoctrine()
+        ->getRepository('AppBundle:User')
+        ->findOneByUsername(app.user.username);
+		
+		
+        return $this->render('profile.html.twig', 
+			array(
+				'username' => $user->getUsername() ,
+				'name'     => $user->getName(),
+				'email'    => $user->getEmail(),
+				'group'    => $user->getGroup()
+			));
+    }
 	
 	
 }
