f5654b89e98b5f181911584aac7d1265e3cf72fc
Lootgvfr <brdmonster@gmail.com>
2016-05-12 17:20:19 +0300

    login

--- src/AppBundle/Controller/SignController.php
+++ src/AppBundle/Controller/SignController.php
@@ -11,42 +11,52 @@ use Symfony\Component\HttpFoundation\Request;
 class SignController extends Controller
 {
     /**
-     * @Route("/sign", name="sign")
+     * @Route("/register", name="register")
      */
-    public function signAction(Request $request)
+    public function registerAction(Request $request)
     {
-		$logger = $this->get('logger');
 		$user = new User();
         $formRegister = $this->createForm(Register::class, $user);
 		if('POST' === $request->getMethod())
 		{
-			if ($request->request->has('register'))
+			$formRegister->handleRequest($request);
+			if ($formRegister->isSubmitted() && $formRegister->isValid()) 
 			{
-				$formRegister->handleRequest($request);
-				if ($formRegister->isSubmitted() && $formRegister->isValid()) 
-				{
-					$password = $this->get('security.password_encoder')
-						->encodePassword($user, $user->getPlainPassword());
-					$user->setPassword($password);
-					$user->setGroup("User");
-					
-					$em = $this->getDoctrine()->getManager();
-					$em->persist($user);
-					$em->flush();
-					
-					return $this->redirectToRoute('sign');
-				}
-			}
-			else if ($request->request->has('login'))
-			{
-				// LOGIN FORM HERE
+				$password = $this->get('security.password_encoder')
+					->encodePassword($user, $user->getPlainPassword());
+				$user->setPassword($password);
+				$user->setGroup("User");
+				
+				$em = $this->getDoctrine()->getManager();
+				$em->persist($user);
+				$em->flush();
+				return $this->redirectToRoute('login');
 			}
 		}
 
-        return $this->render('sign.html.twig', 
+        return $this->render('register.html.twig', 
 			array('form' => $formRegister->createView())
 			);
     }
 	
+	/**
+     * @Route("/login", name="login")
+     */
+    public function loginAction(Request $request)
+    {
+        $authenticationUtils = $this->get('security.authentication_utils');
+
+		$error = $authenticationUtils->getLastAuthenticationError();
+
+		$lastUsername = $authenticationUtils->getLastUsername();
+
+		return $this->render(
+			'login.html.twig',
+			array(
+				'last_username' => $lastUsername,
+				'error'         => $error,
+			)
+		);
+    }
 }
 ?>
