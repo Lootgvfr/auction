1102b38669826f08c0a9f3b6fab00f82d282c288
FanryLight <fanrylight@gmail.com>
2016-05-12 17:02:47 +0300

    login01

--- src/AppBundle/Controller/SignController.php
+++ src/AppBundle/Controller/SignController.php
@@ -42,9 +42,20 @@ class SignController extends Controller
 				// LOGIN FORM HERE
 			}
 		}
+			$authenticationUtils = $this->get('security.authentication_utils');
+
+		// get the login error if there is one
+		$error = $authenticationUtils->getLastAuthenticationError();
+
+		// last username entered by the user
+		$lastUsername = $authenticationUtils->getLastUsername();
+
 
         return $this->render('sign.html.twig', 
-			array('form' => $formRegister->createView())
+			array('form' => $formRegister->createView(),
+			'last_username' => $lastUsername,
+            'error'         => $error
+			)
 			);
     }
 	
