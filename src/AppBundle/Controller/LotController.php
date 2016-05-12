484e2bdf472961ce5e194d2116770cecb88767a2
Lootgvfr <brdmonster@gmail.com>
2016-05-12 17:27:57 +0300

    123

---
+++ src/AppBundle/Controller/LotController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+class LotController extends Controller
+{
+	/**
+     * @Route("/make_lot", name="make-lot")
+     */
+    public function makeLotAction(Request $request)
+    {
+		
+        return $this->render('make-lot.html.twig', array(
+        ));
+    }
+
+	/**
+     * @Route("/lots/{name}", name="lot")
+     */
+    public function lotAction(Request $request, $name)
+    {
+		$found = false;
+		$lot = [];
+		for ($i = 0; $i < count($this::$lots); $i++)
+		{
+			if ($this::$lots[$i]["name"] == $name)
+			{
+				$lot = $this::$lots[$i];
+				$found = true;
+			}
+		}
+        return $this->render('lot.html.twig', array(
+			"found" => $found,
+			"lot" => $lot
+        ));
+    }
+}
+?>
