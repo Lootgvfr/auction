aa338b60967a38c861bc87a3b5721717daaa3c98
FanryLight <fanrylight@gmail.com>
2016-05-21 23:49:56 +0300

    profile

--- src/AppBundle/Controller/ProfileController.php
+++ src/AppBundle/Controller/ProfileController.php
@@ -4,6 +4,8 @@ namespace AppBundle\Controller;
 
 use AppBundle\Form\Edit_profile;
 use AppBundle\Entity\User;
+use AppBundle\Entity\CommentUser;
+use AppBundle\Form\CommentUserForm;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
@@ -16,12 +18,15 @@ class ProfileController extends Controller
      */
     public function editAction(Request $request)
     {
+		
 		$securityContext = $this->container->get('security.authorization_checker');
 		if (!$securityContext->isGranted('IS_AUTHENTICATED_FULLY')) {
-			$this->redirectToRoute('login');
+			
+			return $this->redirectToRoute('login');
 		}
+		
 		$user =  $this->get('security.token_storage')->getToken()->getUser();
-
+		
 		
 		$formEdit = $this->get('form.factory')->create(new Edit_profile(), $user, array(
 			'user' => $user
@@ -36,7 +41,7 @@ class ProfileController extends Controller
 				
 				$em->flush();
 				
-				return $this->redirectToRoute('edit');
+				return $this->redirectToRoute('profile');
 			}
 		}
 		
@@ -45,13 +50,42 @@ class ProfileController extends Controller
 			);
     }
 	
+	/**
+     * @Route("/profile/set_group", name="set_group")
+     */
+    public function setGroupAction(Request $request)
+    {
+		if('POST' === $request->getMethod())
+		{
+			$em = $this->getDoctrine()->getManager();
+			$user = $this->getDoctrine()
+			->getRepository('AppBundle:User')
+			->findOneByUsername($_POST['username']);
+			$user->setGroup($_POST['new_group']);
+				
+			$em->flush();
+		}
+		return $this->redirectToRoute('profile',  array('username' => $_POST['username']));
+	}
 	
 	
     /**
      * @Route("/profile/{username}", name="profile")
      */
-    public function profileAction($username)
+    public function profileAction($username = NULL)
     {
+		if (!$username)
+		{
+			$securityContext = $this->container->get('security.authorization_checker');
+			if (!$securityContext->isGranted('IS_AUTHENTICATED_FULLY')) {
+				
+				return $this->redirectToRoute('login');
+			}
+			$user =  $this->get('security.token_storage')->getToken()->getUser();
+			$username = $user->getUsername();
+			return $this->redirectToRoute('profile',  array('username' => $username));
+		}
+		else 
 		$user = $this->getDoctrine()
         ->getRepository('AppBundle:User')
         ->findOneByUsername($username);
@@ -63,8 +97,60 @@ class ProfileController extends Controller
 				'name'     => $user->getName(),
 				'email'    => $user->getEmail(),
 				'group'    => $user->getGroup(),
-				'path'	   => $user->getWebPath()
+				'path'	   => $user->getWebPath(),
+				'address'  => $user->getAddress(),
+				'phone'    => $user->getPhone(),
+				'info'     => $user->getInfo(),
+				'id'       => $user->getId()
 			));
+    }
+	
+	
+	public function commentNewAction(Request $request, User $seller)
+    {
+        $form = $this->createForm(new CommentUserForm());
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            /** @var Comment $comment */
+            $comment = $form->getData();
+            $comment->setAuthor($this->getUser());
+			$comment->setSeller($seller);
+
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($comment);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('profile', array('username' => $seller->username()));
+        }
+
+        return $this->render('blog/comment_form_error.html.twig', array(
+            'post' => $post,
+            'form' => $form->createView(),
+        ));
+    }
+
+    /**
+     * This controller is called directly via the render() function in the
+     * blog/post_show.html.twig template. That's why it's not needed to define
+     * a route name for it.
+     *
+     * The "id" of the Post is passed in and then turned into a Post object
+     * automatically by the ParamConverter.
+     *
+     * @param Post $post
+     *
+     * @return Response
+     */
+    public function commentFormAction(Post $post)
+    {
+        $form = $this->createForm(new CommentType());
+
+        return $this->render('blog/_comment_form.html.twig', array(
+            'post' => $post,
+            'form' => $form->createView(),
+        ));
     }
 	
 	
---
+++ src/AppBundle/Form/CommentUserForm.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace AppBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+
+class CommentUserForm extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+			 ->add('text', TextareaType::class, array(
+				'attr' => array('placeholder' => 'Text', 'class' => 'textarea'),
+				'label' => false, 'required' => false
+				))
+			
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'AppBundle\Entity\CommentUser',
+        ));
+    }
+}
