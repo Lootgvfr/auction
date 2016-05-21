7325b95d83c9d5d942fdb347917cbea950494d6a
Lootgvfr <brdmonster@gmail.com>
2016-05-21 23:27:08 +0300

    lot durations

--- src/AppBundle/Controller/LotController.php
+++ src/AppBundle/Controller/LotController.php
@@ -32,6 +32,7 @@ class LotController extends Controller
 		$description = '';
 		$starting_price = '';
 		$buyout_price = '';
+		$duration = 0;
 		$cat = $this->getDoctrine()->getRepository('AppBundle:Category')->findOneByName($category);
 		$props = $cat->getProperties();
 		
@@ -56,6 +57,7 @@ class LotController extends Controller
 				$starting_price = $request->request->get('Starting_price');
 				$buyout_price = $request->request->get('Buyout_price');
 				
+				
 				$em = $this->getDoctrine()->getManager();
 				if ($request->request->get('Name') == '' or $request->request->get('Description') == '' or $request->request->get('Starting_price') == '')
 				{
@@ -79,9 +81,6 @@ class LotController extends Controller
 				}
 				$lot->setStatus('Unconfirmed');
 				$lot->setAuthor($this->get('security.token_storage')->getToken()->getUser());
-				$now = new \DateTime();
-				$lot->setStartDate($now);
-				$lot->setEndDate($now);
 				$lot->setCategory($cat);
 				$data = $this->getRequest()->request->all();
 				$file = $this->getRequest()->files->get('file');
@@ -105,6 +104,17 @@ class LotController extends Controller
 					$val->setLot($lot);
 					$em->persist($val);
 				}
+				$duration = intval($request->request->get('Duration'));
+				if ($duration < 1)
+				{
+					throw new \Exception('Duration must be 1 day at least');
+				}
+				$now = new \DateTime();
+				$lot->setStartDate($now);
+				$diff = new \DateInterval("P" . strval($duration) . "D");
+				$end = new \DateTime();
+				$end->add($diff);
+				$lot->setEndDate($end);
 				
 				$em->persist($lot);
 				$em->flush();
@@ -127,124 +137,8 @@ class LotController extends Controller
 			'starting_price' => $starting_price,
 			'buyout_price' => $buyout_price,
 			'edit' => false,
-			'found' => true
-        ));
-    }
-	
-	/**
-     * @Route("/make_lot_choose", name="make-lot")
-     */
-    public function makeLotChooseAction(Request $request)
-    {
-		if( !$this->container->get('security.context')->isGranted('IS_AUTHENTICATED_FULLY') )
-		{
-			return $this->redirectToRoute('login');
-		}
-		$categories = $this->getDoctrine()->getRepository('AppBundle:Category')->findAll();
-        return $this->render('make-lot-choose.html.twig', array(
-			"categories" => $categories
-        ));
-    }
-	
-	/**
-     * @Route("/lots/{id}/delete", name="delete-lot")
-     */
-    public function deleteLotAction(Request $request, $id)
-    {
-		$em = $this->getDoctrine()->getManager();
-		$lot = $this->getDoctrine()->getRepository('AppBundle:Lot')->findOneById($id);
-		$em->remove($lot);
-		$em->flush();
-        return $this->redirectToRoute('home');
-    }
-
-	/**
-     * @Route("/lots/{id}", name="lot")
-     */
-    public function lotAction(Request $request, $id)
-    {
-		$lot = $this->getDoctrine()->getRepository('AppBundle:Lot')->findOneById($id);
-		$error = '';
-		$price = 0;
-		if ($lot)
-		{
-			$found = true;
-			$properties = [];
-			$values = $lot->getValues();
-			$author = $lot->getAuthor();
-			for($i = 0; $i < count($values); $i++)
-			{
-				$name = $values[$i]->getProperty()->getName();
-				$val = $values[$i]->getVal();
-				array_push($properties, array('name' => $name, 'value' => $val) );
-			}
-			$lot->getCurrentPrice();
-		}
-		else
-		{
-			$found = false;
-			$author = null;
-			$properties = [];
-		}
-		if('POST' === $request->getMethod())
-		{
-			if ($request->request->has('Bid'))
-			{
-				try
-				{
-					$v = $request->request->get('Bid_value');
-					$val = floatval($v);
-					if ($val < $lot->getCurrentPrice()*1.02)
-					{
-						throw new \Exception('Your bid must be higher than the current price by 2% at least.');
-					}
-					$bid = new Bid();
-					$bid->setValue($val);
-					$bid->setLot($lot);
-					$bid->setUser($this->get('security.token_storage')->getToken()->getUser());
-					$bid->setDate(new \DateTime());
-					$em = $this->getDoctrine()->getManager();
-					$em->persist($bid);
-					$em->persist($lot);
-					$em->flush();
-					$price = $val;
-				}
-				catch (\Exception $e)
-				{
-					$error = $e->getMessage();
-				}
-			}
-			else if ($request->request->has('Buyout'))
-			{
-				try
-				{
-					$bid = new Bid();
-					$bid->setValue($lot->getBuyoutPrice());
-					$bid->setLot($lot);
-					$bid->setUser($this->get('security.token_storage')->getToken()->getUser());
-					$bid->setDate(new \DateTime());
-					$lot->setEndDate(new \DateTime());
-					$lot->setStatus('Finished');
-					$lot->setCurrentPrice($lot->getBuyoutPrice());
-					$em = $this->getDoctrine()->getManager();
-					$em->persist($bid);
-					$em->persist($lot);
-					$em->flush();
-					$price = $lot->getBuyoutPrice();
-				}
-				catch (\Exception $e)
-				{
-					$error = $e->getMessage();
-				}
-			}
-		}
-        return $this->render('lot.html.twig', array(
-			"found" => $found,
-			"lot" => $lot,
-			'properties' => $properties,
-			'author' => $author,
-			'error' => $error,
-			'price' => $price
+			'found' => true,
+			'duration' => $duration
         ));
     }
 	
@@ -266,8 +160,9 @@ class LotController extends Controller
 			$description = '';
 			$starting_price = '';
 			$buyout_price = '';
+			$duration = 0;
 			$cat = null;
-			$propeties = [];
+			$properties = [];
 		}
 		else
 		{
@@ -279,6 +174,7 @@ class LotController extends Controller
 			$buyout_price = $lot->getBuyoutPrice();
 			$cat = $lot->getCategory();
 			$vals = $lot->getValues();
+			$duration = date_diff($lot->getStartDate(), $lot->getEndDate())->days;
 			
 			$properties = [];
 			for ($i = 0; $i < count($vals); $i++)
@@ -308,6 +204,7 @@ class LotController extends Controller
 					{
 						throw new \Exception('Some of the fields are empty');
 					}
+					
 					$lot->setName($request->request->get('Name'));
 					$lot->setDescription($request->request->get('Description'));
 					$lot->setStartPrice($request->request->get('Starting_price'));
@@ -341,7 +238,18 @@ class LotController extends Controller
 						$vals[$i]->setVal($str);
 						$em->persist($vals[$i]);
 					}
-					
+					$duration = intval($request->request->get('Duration'));
+					if ($duration < 1)
+					{
+						throw new \Exception('Duration must be 1 day at least');
+					}
+					$now = new \DateTime();
+					$lot->setStartDate($now);
+					$diff = new \DateInterval("P" . strval($duration) . "D");
+					$end = new \DateTime();
+					$end->add($diff);
+					$lot->setEndDate($end);
+				
 					$em->persist($lot);
 					$em->flush();
 					
@@ -365,7 +273,170 @@ class LotController extends Controller
 			'buyout_price' => $buyout_price,
 			'edit' => true,
 			'found' => $found,
-			'id' => $id
+			'id' => $id,
+			'duration' => $duration
+        ));
+    }
+	
+	/**
+     * @Route("/make_lot_choose", name="make-lot")
+     */
+    public function makeLotChooseAction(Request $request)
+    {
+		if( !$this->container->get('security.context')->isGranted('IS_AUTHENTICATED_FULLY') )
+		{
+			return $this->redirectToRoute('login');
+		}
+		$categories = $this->getDoctrine()->getRepository('AppBundle:Category')->findAll();
+        return $this->render('make-lot-choose.html.twig', array(
+			"categories" => $categories
+        ));
+    }
+	
+	/**
+     * @Route("/lots/{id}/delete", name="delete-lot")
+     */
+    public function deleteLotAction(Request $request, $id)
+    {
+		if( !$this->container->get('security.context')->isGranted('IS_AUTHENTICATED_FULLY') )
+		{
+			return $this->redirectToRoute('login');
+		}
+		$em = $this->getDoctrine()->getManager();
+		$lot = $this->getDoctrine()->getRepository('AppBundle:Lot')->findOneById($id);
+		$em->remove($lot);
+		$em->flush();
+        return $this->redirectToRoute('home');
+    }
+	
+	/**
+     * @Route("/lots/{id}/confirm", name="confirm-lot")
+     */
+    public function confirmLotAction(Request $request, $id)
+    {
+		if( !$this->container->get('security.context')->isGranted('IS_AUTHENTICATED_FULLY') )
+		{
+			return $this->redirectToRoute('login');
+		}
+		$em = $this->getDoctrine()->getManager();
+		$lot = $this->getDoctrine()->getRepository('AppBundle:Lot')->findOneById($id);
+		$lot->setStatus("Open");
+		$diff = date_diff($lot->getStartDate(), $lot->getEndDate());
+		$now = new \DateTime();
+		$lot->setStartDate($now);
+		$now = $now->add($diff);
+		$lot->setEndDate($now);
+		$em->persist($lot);
+		$em->flush();
+        return $this->redirectToRoute('lot', array('id'=>$id));
+    }
+
+	/**
+     * @Route("/lots/{id}", name="lot")
+     */
+    public function lotAction(Request $request, $id)
+    {
+		$lot = $this->getDoctrine()->getRepository('AppBundle:Lot')->findOneById($id);
+		$error = '';
+		$price = 0;
+		if ($lot)
+		{
+			$found = true;
+			$properties = [];
+			$values = $lot->getValues();
+			$author = $lot->getAuthor();
+			for($i = 0; $i < count($values); $i++)
+			{
+				$name = $values[$i]->getProperty()->getName();
+				$val = $values[$i]->getVal();
+				array_push($properties, array('name' => $name, 'value' => $val) );
+			}
+			$lot->getCurrentPrice();
+			$duration = date_diff($lot->getStartDate(), $lot->getEndDate())->days;
+		}
+		else
+		{
+			$found = false;
+			$author = null;
+			$properties = [];
+			$duration = 0;
+		}
+		if('POST' === $request->getMethod())
+		{
+			if ($request->request->has('Bid'))
+			{
+				try
+				{
+					$em = $this->getDoctrine()->getManager();
+					$now = new \DateTime();
+					if ($now > $lot->getEndDate())
+					{
+						$lot->setStatus('Finished');
+						$em->persist($lot);
+						$em->flush();
+						throw new \Exception('This lot is already closed');
+					}
+					$v = $request->request->get('Bid_value');
+					$val = floatval($v);
+					if ($val < $lot->getCurrentPrice()*1.02)
+					{
+						throw new \Exception('Your bid must be higher than the current price by 2% at least.');
+					}
+					$bid = new Bid();
+					$bid->setValue($val);
+					$bid->setLot($lot);
+					$bid->setUser($this->get('security.token_storage')->getToken()->getUser());
+					$bid->setDate(new \DateTime());
+					$em->persist($bid);
+					$em->persist($lot);
+					$em->flush();
+					$price = $val;
+				}
+				catch (\Exception $e)
+				{
+					$error = $e->getMessage();
+				}
+			}
+			else if ($request->request->has('Buyout'))
+			{
+				try
+				{
+					$em = $this->getDoctrine()->getManager();
+					$now = new \DateTime();
+					if ($now > $lot->getEndDate())
+					{
+						$lot->setStatus('Finished');
+						$em->persist($lot);
+						$em->flush();
+						throw new \Exception('This lot is already closed');
+					}
+					$bid = new Bid();
+					$bid->setValue($lot->getBuyoutPrice());
+					$bid->setLot($lot);
+					$bid->setUser($this->get('security.token_storage')->getToken()->getUser());
+					$bid->setDate(new \DateTime());
+					$lot->setEndDate(new \DateTime());
+					$lot->setStatus('Finished');
+					$lot->setCurrentPrice($lot->getBuyoutPrice());
+					$em->persist($bid);
+					$em->persist($lot);
+					$em->flush();
+					$price = $lot->getBuyoutPrice();
+				}
+				catch (\Exception $e)
+				{
+					$error = $e->getMessage();
+				}
+			}
+		}
+        return $this->render('lot.html.twig', array(
+			"found" => $found,
+			"lot" => $lot,
+			'properties' => $properties,
+			'author' => $author,
+			'error' => $error,
+			'price' => $price,
+			'duration' => $duration
         ));
     }
 }
